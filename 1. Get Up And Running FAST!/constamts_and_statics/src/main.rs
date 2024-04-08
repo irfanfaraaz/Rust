828afe4ec6e961bdbec8e84b01d2b21ecc361f75
irfanfaraaz <irfanfaraaz03@gmail.com>
2024-04-09 01:31:51 +0530

    functions

---
+++ 1. Get Up And Running FAST!/constamts_and_statics/src/main.rs
@@ -0,0 +1,11 @@
+const MAX_PLAYERS: u8 = 10;
+static CASINO_NAME: &str = "Rusty Casino";
+
+fn main() {
+    let a = 10;
+    let b = MAX_PLAYERS;
+
+    let c = CASINO_NAME;
+    let d = CASINO_NAME;
+}
+
