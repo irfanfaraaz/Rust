50c3ce68ca41d1352c7bef88b4c8a1967176f8a5
irfanfaraaz <irfanfaraaz03@gmail.com>
2024-04-09 15:43:53 +0530

    control flow

---
+++ 1. Get Up And Running FAST!/control_flow/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let mut a = 10;
+    if a>5 {
+        println!("a is greater than 5");
+    }
+
+    else if a<5 {
+        println!("a is less than 5");
+    }
+
+    else{
+        println!("a is equal to 5");
+    }
+
+    let b:i32 = if a>5 {
+        1
+    }
+    else{
+        0
+    };
+    println!("b is {}", b);
+
+    // loops
+    loop{
+        println!("Infinite loop");
+        break;
+    }
+    // labeling loops
+    'outer: loop{
+        println!("Infinite loop");
+        loop{
+            println!("Inner loop");
+            break 'outer;
+        }
+    }
+
+    while a<15{
+        println!("a is {}", a);
+        a+=1;
+    }
+
+    let a = [1,2,3,4,5];
+    for i in a{
+        println!("i is {}", i);
+    }
+
+
+}
