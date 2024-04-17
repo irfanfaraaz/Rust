febe0aea5069faa9e2e4bbfea2404958135ad6de
irfanfaraaz <irfanfaraaz03@gmail.com>
2024-04-17 23:43:24 +0530

    trait object

--- 2. Intermediate/1. Polymorphism With Generics & Traits/traits_and_traitBounds/src/main.rs
+++ 2. Intermediate/1. Polymorphism With Generics & Traits/traits_and_traitBounds/src/main.rs
@@ -1,47 +1,47 @@
-trait Park {
+trait Vehicle: Paint {
     fn park(&self);
+    fn get_default_color() -> String {
+        "black".to_owned()
+    }
 }
 
 trait Paint {
     fn paint(&self, color: String) {
-        println!("Painting vehicle with color: {}", color);
+        println!("painting object: {}", color);
     }
 }
+
 struct VehicleInfo {
     make: String,
     model: String,
-    year: u32,
+    year: u16,
 }
+
 struct Car {
-    // make: String,
-    // model: String,
-    // year: u32,
     info: VehicleInfo,
 }
-impl Park for Car {
+
+impl Vehicle for Car {
     fn park(&self) {
-        println!("Parking car");
+        println!("parking car!");
     }
 }
 
 impl Paint for Car {}
 
 struct Truck {
-    // make: String,
-    // model: String,
-    // year: u32,
     info: VehicleInfo,
 }
 
 impl Truck {
     fn unload(&self) {
-        println!("Unloading truck");
+        println!("unloading truck.")
     }
 }
 
-impl Park for Truck {
+impl Vehicle for Truck {
     fn park(&self) {
-        println!("Parking truck");
+        println!("parking truck!");
     }
 }
 
@@ -51,46 +51,51 @@ struct House {}
 
 impl Paint for House {
     fn paint(&self, color: String) {
-        println!("Painting house with color: {}", color);
+        println!("painting house: {}", color);
     }
 }
 
 fn main() {
     let car = Car {
         info: VehicleInfo {
-            make: "Toyota".to_string(),
-            model: "Camry".to_string(),
-            year: 2018,
+            make: "Honda".to_owned(),
+            model: "Civic".to_owned(),
+            year: 1995,
         },
     };
-
     let house = House {};
+    let object = create_paintable_object(true);
 
-    let object = create_paintable_object();
+    let paintable_objects: Vec<&dyn Paint> = vec![&car, &house];
 
     paint_red(&car);
     paint_red(&house);
-    paint_red(&object);
+    paint_red(object.as_ref());
+
     paint_vehicle_red(&car);
-    // paint_vehicle_red(&house); errors coz house does not implement Park
-    // paint_vehicle_red(&object); errors coz house does not implement Park
 }
 
-fn paint_red<T: Paint>(object: &T) {
-    object.paint("red".to_string());
-}
-
-fn paint_red2(object: &impl Paint) {
-    object.paint("red".to_string());
+fn paint_red(object: &dyn Paint) {
+    object.paint("red".to_owned());
 }
 
 fn paint_vehicle_red<T>(object: &T)
 where
-    T: Paint + Park,
+    T: Vehicle,
 {
-    object.paint("red".to_string());
+    object.paint("red".to_owned());
 }
 
-fn create_paintable_object() -> impl Paint {
-    House {}
+fn create_paintable_object(vehicle: bool) -> Box<dyn Paint> {
+    if vehicle {
+        Box::new(Car {
+            info: VehicleInfo {
+                make: "Honda".to_owned(),
+                model: "Civic".to_owned(),
+                year: 1995,
+            },
+        })
+    } else {
+        Box::new(House {})
+    }
 }
